4978ff3d67c71c13c823ce9a7267601443ccfbc3
brian <brian.oliveira100@gmail.com>
2020-03-30 17:02:30 -0400

    Versão 1.0

---
+++ precache-manifest.969edbebda4aaed22bb8180a98a1de12.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6805bcde7a1d9399d18732958e78589b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd684691a5f23899eaad",
+    "url": "/static/css/main.28f568b3.chunk.css"
+  },
+  {
+    "revision": "bb36b2bd56e6f65167e3",
+    "url": "/static/js/2.ee29a942.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ee29a942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd684691a5f23899eaad",
+    "url": "/static/js/main.143a13a5.chunk.js"
+  },
+  {
+    "revision": "79843e62424d7b8903d8",
+    "url": "/static/js/runtime-main.12b1aa45.js"
+  },
+  {
+    "revision": "7728da742f84654587a36f780adf9152",
+    "url": "/static/media/backgroundPath.7728da74.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "62e76a53668c174b367ad45428ee8728",
+    "url": "/static/media/logo.62e76a53.svg"
+  }
+]);
